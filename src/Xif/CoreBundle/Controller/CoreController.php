eac8bc395a750cd5eea1f90efd7c4b17aeb02531
Nils Van Zuijlen <nilsdu29@gmail.com>
2016-11-24 16:45:38 +0100

    Update CoreController.php

--- src/Xif/CoreBundle/Controller/CoreController.php
+++ src/Xif/CoreBundle/Controller/CoreController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Xif\CoreBundle\FormModels\ContactModel;
-use Xif\CoreBundle\Form\ContactType;
+use Xif\CoreBundle\Form\Type\ContactType;
 
 /**
  * Controlleur du CoreBundle
