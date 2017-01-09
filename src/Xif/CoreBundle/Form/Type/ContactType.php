7b749976110e0a8303ebd57777e0132bb27f59f2
Nils Van Zuijlen <nilsdu29@gmail.com>
2017-01-09 16:49:59 +0100

    Update ContactType.php

--- src/Xif/CoreBundle/Form/Type/ContactType.php
+++ src/Xif/CoreBundle/Form/Type/ContactType.php
@@ -4,9 +4,7 @@
 namespace Xif\CoreBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
